52bc20f7a48ed81250b9d28ad3066ed97d06e79f
Robin Hahling <robin.hahling@gw-computing.net>
2011-10-21 16:21:11 +0200

    initial commit

---
+++ main.c
@@ -0,0 +1,106 @@
+/*
+ * main.c
+ *
+ * (C) 2011 - Hahling Robin <robin.hahling@gw-computing.net>
+ *
+ * Displays free disk space in an elegant manner with color support
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/param.h>
+
+/* structure to store infos from /etc/mtab */
+struct mntdev {
+	char device[MAXPATHLEN];
+	char mntpt[MAXPATHLEN];
+	char fstype[MAXPATHLEN};
+};
+
+/* set flags for options */
+int hflag, Hflag, vflag, Vflag;
+
+/* functions declaration */
+static void colormsg(FILE *output, unsigned int color);
+static int display(void);
+struct mntpt getmntpt(FILE *mtab);
+static void usage(void);
+
+int
+main(int argc, char *argv[])
+{
+	int ch;
+
+	while ((ch = getopt(argc, argv, "hHvV:")) != -1) {
+		switch (ch) {
+		case 'h':
+			hflag = 1;
+			break;
+		case 'H':
+			Hflag = 1;
+			break;
+		case 'v':
+			vflag = 1;
+			break;
+		case 'V':
+			Vflag = 1;
+			break;
+		case '?':
+		default:
+			usage();
+			/* NOTREACHED */
+		}
+	}
+}
+
+static void
+colormsg(FILE *output, unsigned int color)
+{
+
+}
+
+static int
+display(void)
+{
+
+}
+
+struct mntdev
+getmntpt(FILE *mtab)
+{
+	struct mntdev crtdev = {"","",""};
+
+	if (fscanf(mtab, "%s %s %s", crtdev.device, crtdev.mntpt, crtdev.fstype) == EOF) {
+		/* error occured */
+		if (ferror(mtab) != 0) {
+			perror("Error while reading mtab");
+			(void)fclose(mtab);
+			exit(EXIT_FAILURE);
+			/* NOTREACHED */
+		}
+	}
+
+	/* in case of EOF, crtdev does not change (length of elt = 0) */
+	return crtdev;
+	/* NOTREACHED */
+}
+
+getfileusg()
+{
+
+}
+
+static void
+usage(void)
+{
+	(void)fputs("Usage: dfc [OPTIONS(S)]\n"
+		"Available options:\n"
+		"	-h	print size in human readable format\n"
+		"	-H	print size in human readable format but using powers of 1000, not 1024\n"
+		"	-v	verbose mode\n"
+		"	-V	print program version\n",
+		stderr)
+
+	exit(EXIT_FAILURE);
+	/* NOTREACHED */
+}
