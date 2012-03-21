1ae0618616e35f00be2240e2f5c3dfe68b468bd1
Rolinh - Robin Hahling <robin.hahling@gw-computing.net>
2012-03-21 20:47:05 +0100

    fixes the bug about str not being truncated

--- dfc.h
+++ dfc.h
@@ -50,6 +50,6 @@ void disp(struct list lst);
 void disp_header(struct list *lst);
 double cvrt(double nb);
 int imax(int a, int b);
-char* trk(char *fsname);
+char* trk(char *str);
 
 #endif /* ndef DFC_H */
