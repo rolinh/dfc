5436bb0eafef558bf9928bb96d0766021cbdc241
Rolinh - Robin Hahling <robin.hahling@gw-computing.net>
2012-04-01 14:25:47 +0200

    reorganised code: fix #68 (dfc is too monolithic

--- src/list.h
+++ src/list.h
@@ -25,35 +25,57 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef DFC_H
-#define DFC_H
+#ifndef LIST_H
+#define LIST_H
 /*
- * dfc.h
+ * list.h
  *
- * header file for dfc.c
+ * list structure
  */
 
-#include "list.h"
+/*
+ * structure needed to store informations about mounted fs
+ * It should contain brut datas.
+ * Later on, we would need to compute those infos:
+ * unsigned long bused;	used blocks
+ * double prct_used;	usage of fs in percent
+ * double prct_free;	free space of fs in percent
+ */
+struct fsmntinfo {
+	/* infos to get from getmntent(3) */
+	char *fsname;	/* name of mounted file system */
+	char *dir;	/* file system path prefix */
+	char *type;	/* mount type */
+	char *opts;	/* mount options (see mntent.h) */
 
-#define VERSION "2.3.0"
+	/* infos to get from statvfs(3) */
+	unsigned long bsize;	/* file system block size */
+	unsigned long frsize;	/* fragment size */
+	unsigned long blocks;	/* size of fs in frsize unit */
+	unsigned long bfree;	/* # of free blocks */
+	unsigned long bavail;	/* # of available blocks */
+	unsigned long files;	/* # of inodes */
+	unsigned long ffree;	/* # of free inodes */
+	unsigned long favail;	/* # of available inodes */
+
+	/* pointer to the next element of the list */
+	struct fsmntinfo *next;
+};
+
+/* list structure to store fsmntinfo */
+struct list {
+	struct fsmntinfo *head;
+	struct fsmntinfo *tail;
+
+	int fsmaxlen; /* should be the size of the longest fsname */
+	int dirmaxlen; /* same for dir */
+	int typemaxlen; /* same for type */
+};
 
 /* function declaration */
-static void usage(int status);
-void fetch_info(struct list *lst);
-void disp(struct list *lst, char *fsfilter);
-void disp_header(struct list *lst);
-void disp_sum(struct list *lst, double stot, double utot, double ftot,
-		double ifitot, double ifatot);
-void disp_bar(double perct);
-void disp_at(double n, double perct);
-void disp_perct(double perct);
-void change_color(double perct);
-void reset_color(void);
-double cvrt(double n);
-void humanize(double n, double perct);
-int imax(int a, int b);
-char* trk(char *str);
-char* shortenstr(char *str, int len);
-unsigned int getttywidth(void);
+void init_queue(struct list *lst);
+int is_empty(struct list lst);
+int enqueue(struct list *lst, struct fsmntinfo elt);
+struct fsmntinfo fmi_init(void);
 
-#endif /* ndef DFC_H */
+#endif /* ndef LIST_H */
