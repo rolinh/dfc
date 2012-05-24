rolinh/dfc

commit 4053c8459aa17262f6af61351acec0ded240a7a8

macros should not go over 80 columns. Also, rewrote a loop in sanitize_string to make it more readable

src/util.c
@@ -122,12 +122,12 @@ shortenstr(char *str, int len)
 char *
 sanitizestr(const char *str)
 {
-	int i;
-	size_t nchars = 1; /* Trailing \0 */
+	int i = 0;
 	int j = 0;
+	size_t nchars = 1; /* Trailing \0 */
 	char *new;
 
-	for (i = 0; str[i] != '\0'; i++) {
+	while (str[i++] != '\0') {
 		if (str[i] == '_')
 			nchars += 2;
 		else

po/fr.po
@@ -7,7 +7,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: dfc 3.0.0-devel\n"
 "Report-Msgid-Bugs-To: robin.hahling@gw-computing.net\n"
-"POT-Creation-Date: 2012-05-23 21:05+0200\n"
+"POT-Creation-Date: 2012-05-24 00:14+0200\n"
 "PO-Revision-Date: 2012-04-15 13:52+0200\n"
 "Last-Translator: Robin Hahling <robin.hahling@gw-computing.net>\n"
 "Language-Team: French\n"
@@ -72,36 +72,36 @@ msgstr ",OPTIONS DE MONTAGE"
 msgid "SUM:,"
 msgstr "SOMME:,"
 
-#: src/dfc.c:200
+#: src/dfc.c:201
 #, c-format
 msgid "-c: illegal sub option %s\n"
 msgstr "-c: sous-option illégale %s\n"
 
-#: src/dfc.c:233
+#: src/dfc.c:234
 #, c-format
 msgid "-e: illegal sub option %s\n"
 msgstr "-e: sous-option illégale %s\n"
 
-#: src/dfc.c:281
+#: src/dfc.c:282
 #, c-format
 msgid "-q: illegal sub option %s\n"
 msgstr "-q: sous-option illégale %s\n"
 
-#: src/dfc.c:335
+#: src/dfc.c:336
 #, c-format
 msgid "-u: illegal sub option %s\n"
 msgstr "-u: sous-option illégale %s\n"
 
-#: src/dfc.c:377
+#: src/dfc.c:378
 #, c-format
 msgid "Error reading the configuration file: %s\n"
 msgstr "Erreur pendant la lecture du fichier de configuration: %s\n"
 
-#: src/dfc.c:412
+#: src/dfc.c:413
 msgid "Try dfc -h for more information\n"
 msgstr "Essayez dfc -h pour plus d'informations\n"
 
-#: src/dfc.c:415
+#: src/dfc.c:416
 msgid ""
 "Usage:  dfc [OPTIONS(S)] [-c WHEN] [-e FORMAT] [-p FSNAME] [-q SORTBY] [-t "
 "FSTYPE]\n"
@@ -137,7 +137,7 @@ msgstr ""
 "\t-l\taffiche les informations sur les systèmes de fichiers montés\n"
 "\t\tlocalement uniquement\n"
 
-#: src/dfc.c:433
+#: src/dfc.c:434
 msgid ""
 "\t-m\tuse metric (SI unit)\n"
 "\t-n\tdo not print header\n"
@@ -175,17 +175,17 @@ msgstr ""
 "\t-w\tutilise une barre plus large\n"
 "\t-W\tlong nom de fichiers (dé-tronque)\n"
 
-#: src/dfc.c:500
+#: src/dfc.c:501
 #, c-format
 msgid "WARNING: %s was skipped because it cannot be stated"
 msgstr "AVERTISSEMENT: %s a été sauté car il ne peut pas être lu"
 
-#: src/dfc.c:934
+#: src/dfc.c:945
 #, c-format
 msgid "Could not retrieve mount flags for %s\n"
 msgstr "Impossible de récupérer les options de montage pour %s\n"
 
-#: src/dfc.c:970
+#: src/dfc.c:981
 #, c-format
 msgid "Truncating mount options for %s\n"
 msgstr "Troncation des options de montage pour %s\n"

src/dfc.c
@@ -58,7 +58,8 @@
 #include <sys/param.h>
 #include <sys/statvfs.h>
 
-#if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__APPLE__) || defined(__DragonFly__)
+#if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) \
+	|| defined(__APPLE__) || defined(__DragonFly__)
 #include <sys/ucred.h>
 #include <sys/mount.h>
 #endif /* __FreeBSD__ || __NetBSD__ || __OpenBSD__ || __APPLE__ || __DragonFly__ */
@@ -793,7 +794,8 @@ disp(struct list *lst, char *fstfilter, char *fsnfilter, struct display *sdisp)
 		sdisp->deinit();
 }
 
-#if defined(__DragonFly__) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__APPLE__)
+#if defined(__DragonFly__) || defined(__FreeBSD__) || defined(__NetBSD__) \
+	|| defined(__OpenBSD__) || defined(__APPLE__)
 /*
  * All the flags found in *BSD and Mac OS X, alphabetically sorted.
  */
@@ -804,7 +806,8 @@ struct flag_str {
 #if defined(__FreeBSD__)
 	{ MNT_ACLS,               "acls"               },
 #endif
-#if defined(__DragonFly__) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined (__APPLE__)
+#if defined(__DragonFly__) || defined(__FreeBSD__) || defined(__NetBSD__) \
+	|| defined(__OpenBSD__) || defined (__APPLE__)
 	{ MNT_ASYNC,              "async"              },
 #endif
 #if defined(__APPLE__)
@@ -827,7 +830,8 @@ struct flag_str {
 #if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__DragonFly__)
 	{ MNT_EXPORTANON,         "exportanon"         },
 #endif
-#if defined(__FreeBSD__) || defined(__NetBSD__) ||  defined(__DragonFly__) || defined(__APPLE__)
+#if defined(__FreeBSD__) || defined(__NetBSD__) ||  defined(__DragonFly__) \
+	|| defined(__APPLE__)
 	{ MNT_EXPORTED,           "exported"           },
 #endif
 #if defined(__FreeBSD__) || defined(__NetBSD__)
@@ -842,7 +846,8 @@ struct flag_str {
 #if defined(__APPLE__)
 	{ MNT_JOURNALED,          "journaled"          },
 #endif
-#if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__DragonFly__) || defined(__OpenBSD__) || defined(__APPLE__)
+#if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__DragonFly__) \
+	|| defined(__OpenBSD__) || defined(__APPLE__)
 	{ MNT_LOCAL,              "local"              },
 #endif
 #if defined(__FreeBSD__) || defined(__APPLE__)
@@ -851,7 +856,8 @@ struct flag_str {
 #if defined(__FreeBSD__)
 	{ MNT_NFS4ACLS,           "nfs4acls"           },
 #endif
-#if defined(__DragonFly__) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__APPLE__)
+#if defined(__DragonFly__) || defined(__FreeBSD__) || defined(__NetBSD__) \
+	|| defined(__OpenBSD__) || defined(__APPLE__)
 	{ MNT_NOATIME,            "noatime"            },
 #endif
 #if defined(__FreeBSD__)
@@ -861,24 +867,28 @@ struct flag_str {
 #if defined(__NetBSD__)
 	{ MNT_NOCOREDUMP,         "nocoredump"         },
 #endif
-#if defined(__DragonFly__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__APPLE__)
+#if defined(__DragonFly__) || defined(__NetBSD__) || defined(__OpenBSD__) \
+	|| defined(__APPLE__)
 	{ MNT_NODEV,              "nodev"              },
 #endif
 #if defined(__NetBSD__)
 	{ MNT_NODEVMTIME,         "nodevmtime"         },
 #endif
-#if defined(__DragonFly__) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__APPLE__)
+#if defined(__DragonFly__) || defined(__FreeBSD__) || defined(__NetBSD__) \
+	|| defined(__OpenBSD__) || defined(__APPLE__)
 	{ MNT_NOEXEC,             "noexec"             },
 	{ MNT_NOSUID,             "nosuid"             },
 #endif
 #if defined(__FreeBSD__)
 	{ MNT_NOSYMFOLLOW,        "nosymfollow"        },
 #endif
-#if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__DragonFly__) || defined(__APPLE__)
+#if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__DragonFly__) \
+	|| defined(__APPLE__)
 	{ MNT_QUOTA,              "quota"              },
 #endif
 	/* MNT_RDONLY is treated separately in statfs_flags_to_str(). */
-#if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__DragonFly__) || defined(__APPLE__)
+#if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__DragonFly__) \
+	|| defined(__APPLE__)
 	{ MNT_ROOTFS,             "rootfs"             },
 #endif
 #if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__)
@@ -890,7 +900,8 @@ struct flag_str {
 #if defined(__NetBSD__)
 	{ MNT_SYMPERM,            "symperm"            },
 #endif
-#if defined(__DragonFly__) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__APPLE__)
+#if defined(__DragonFly__) || defined(__FreeBSD__) || defined(__NetBSD__) \
+	|| defined(__OpenBSD__) || defined(__APPLE__)
 	{ MNT_SYNCHRONOUS,        "sync"               },
 #endif
 #if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__APPLE__)

src/dfc.h
@@ -45,16 +45,14 @@ void usage(int status);
 void fetch_info(struct list *lst);
 void disp(struct list *lst, char *fsfilter, char *fsnfilter, struct display *sdisp);
 
-#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__APPLE__) || defined(__DragonFly__)
+#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__APPLE__) \
+	|| defined(__DragonFly__)
 /* avoid struct statfs declared inside parameter list warning */
 struct statfs;
-
 char * statfs_flags_to_str(struct statfs *s);
-#endif /* __FreeBSD__ || __OpenBSD__ || __APPLE__ || __DragonFly__ */
-
-#ifdef __NetBSD__
+#elif defined(__NetBSD__)
 struct statvfs;
 char *statfs_flags_to_str(struct statvfs *s);
-#endif
+#endif /* __FreeBSD__ || __OpenBSD__ || __APPLE__ || __DragonFly__ || __NetBSD__ */
 
 #endif /* ndef DFC_H */

commit 8104126a2835006692f1750e600d26520586a9cd

Fix a loop index in sanitizestr().
This bug was introduced in 4053c845.

Signed-off-by: Cyril Roelandt <tipecaml@gmail.com>

src/util.c
@@ -137,7 +137,7 @@ sanitizestr(const char *str)
 	size_t nchars = 1; /* Trailing \0 */
 	char *new;
 
-	while (str[i++] != '\0') {
+	for (i = 0; str[i] != '\0'; i++) {
 		if (str[i] == '_')
 			nchars += 2;
 		else