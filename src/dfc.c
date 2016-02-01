7a5e368764539108a85f242e3a7b413ae7175944
Robin Hahling <robin.hahling@gw-computing.net>
2016-02-01 19:16:26 +0100

    happy new year!

--- src/dfc.c
+++ src/dfc.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2015, Robin Hahling
+ * Copyright (c) 2012-2016, Robin Hahling
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
